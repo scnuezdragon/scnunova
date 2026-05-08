70a8a267da0125e48fb3231162f79f27ed0c56ee
scnuezdragon <20256059@s.scnu.ac.kr>
2026-05-08 14:45:41 +0900

    Add a program to sum two numbers

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    int num2;
+    int sum;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2; 
+
+    printf("The sum of the two numbers is %d.\n", sum);
+
+    return 0;
+}
